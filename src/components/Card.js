629971f2c9a60d56b04cd4d5df706d1a31a355cb
L. Lester Johnson <shuai.jan28@gmail.com>
2020-03-07 20:05:19 -0500

    update new update accessibility aria-label

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({id, name, city, email}) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
-            <img alt="robots" src={`https://flathash.com/${id}?200x200`} />
+            <img alt="robots" src={`https://flathash.com/${id}?size=200x200`} />
             <div>
                 <h2 value={city}>{name}</h2>
                 <p>{email}</p>
